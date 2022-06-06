1690609029d9d03264045ec6b68ca4de928c7de3
chakrit tnasinpaibool <newchakrit@Administrators-MacBook-Pro-7.local>
2022-06-06 22:37:29 +0700

    ss

--- main.go
+++ main.go
@@ -7,37 +7,60 @@ import (
 	_ "github.com/denisenkom/go-mssqldb"
 )
 
+//  ------ Code style Guard Pattern ------
+type Cover struct {
+	Id   int
+	Name string
+}
+
+var db *sql.DB
+
 func main() {
-	db, err := sql.Open("sqlserver", "sqlserver://sa:P@ssw0rd@13.76.163.73?param1=value&database=techcoach")
+	var err error
+	db, err = sql.Open("sqlserver", "sqlserver://sa:P@ssw0rd@13.76.163.73?param1=value&database=techcoach")
 
 	if err != nil {
 		panic(err)
 	}
 
-	// code style Guard Pattern
-
-	err = db.Ping()
+	covers, err := GetCovers()
 	if err != nil {
-		panic(err)
+		fmt.Print(err)
+		return
 	}
 
-	query := "select * from cover"
+	for _, cover := range covers {
+		fmt.Println(cover)
+	}
+
+}
+
+func GetCovers() ([]Cover, error) {
+	err := db.Ping()
+	if err != nil {
+		return nil, err
+	}
+
+	query := "select id,name from cover" //input colunn name!!
 	// Exec => insert update delete
 	// Query => select mutiple row2
 	// QueryRow => select a row
 	rows, err := db.Query(query)
 
 	if err != nil {
-		panic(err)
+		return nil, err
+	}
+	defer rows.Close()
+
+	covers := []Cover{}
+	for rows.Next() {
+		cover := Cover{}
+		err = rows.Scan(&cover.Id, &cover.Name)
+		if err != nil {
+			return nil, err
+		}
+		covers = append(covers, cover)
 	}
 
-	id := 0
-	name := ""
-	ok := rows.Next()
-
-	if ok {
-		rows.Scan(&id, &name)
-	}
-
-	fmt.Println(id, name)
+	return covers, nil
 }
