fce130fa315ee1120247ecee7cbbb6d6b8898fa6
chakrit tnasinpaibool <newchakrit@Administrators-MacBook-Pro-7.local>
2022-06-06 18:33:46 +0700

    select query

--- main.go
+++ main.go
@@ -2,18 +2,42 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 
 	_ "github.com/denisenkom/go-mssqldb"
 )
 
 func main() {
-	db, err := sql.Open("sqlserver", "sqlserver://sa:P@ssw0rd@127.0.0.1?param1=value&database=test")
+	db, err := sql.Open("sqlserver", "sqlserver://sa:P@ssw0rd@13.76.163.73?param1=value&database=techcoach")
+
 	if err != nil {
 		panic(err)
 	}
+
+	// code style Guard Pattern
 
 	err = db.Ping()
 	if err != nil {
 		panic(err)
 	}
+
+	query := "select * from cover"
+	// Exec => insert update delete
+	// Query => select mutiple row2
+	// QueryRow => select a row
+	rows, err := db.Query(query)
+
+	if err != nil {
+		panic(err)
+	}
+
+	id := 0
+	name := ""
+	ok := rows.Next()
+
+	if ok {
+		rows.Scan(&id, &name)
+	}
+
+	fmt.Println(id, name)
 }
